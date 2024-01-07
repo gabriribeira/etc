086217a2fce120700e235bd76cf6145567874355
gabriribeira <gmrribeira@gmail.com>
2024-01-07 14:41:48 +0000

    Added Default Page Tasks: Missing from last commit

---
+++ src/pages/Tasks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BottomBar from "../components/common/BottomBar";
+
+const Tasks = () => {
+    return (
+      <div>
+        Tasks
+        <BottomBar />
+      </div>
+    );
+  };
+  export default Tasks;
