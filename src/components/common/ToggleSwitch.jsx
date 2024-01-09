4c64f22d28c5c2e0cd8091fd5f978ddc4cb0b76f
gabriribeira <gmrribeira@gmail.com>
2024-01-09 14:30:21 +0000

    Prepared Toggle to receive prop value

--- src/components/common/ToggleSwitch.jsx
+++ src/components/common/ToggleSwitch.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
-const ToggleSwitch = () => {
+const ToggleSwitch = ({ checked }) => {
   const [isChecked, setIsChecked] = useState(false);
 
   const handleToggle = () => {
     setIsChecked(!isChecked);
   };
 
+  useEffect(() => {
+    if (checked) {
+      setIsChecked(true);
+    }
+  }, [checked]);
+
   return (
     <div
-      className={`toggle-switch w-[70px] h-[40px] bg-black40 rounded-full relative pointer ${isChecked ? "checked bg-salmon" : ""}`}
+      className={`toggle-switch w-[70px] h-[40px] bg-black40 rounded-full relative pointer ${
+        isChecked ? "checked bg-salmon" : ""
+      }`}
       onClick={handleToggle}
     >
       <div className="toggle-knob w-[30px] h-[30px] bg-white rounded-full absolute top-[50%] ml-1" />
     </div>
   );
+};
+
+ToggleSwitch.propTypes = {
+  checked: PropTypes.bool,
 };
 
 export default ToggleSwitch;
