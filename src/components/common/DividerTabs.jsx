42921d76a7aea307337b7037af0f7b28c366905b
gabriribeira <gmrribeira@gmail.com>
2024-01-09 23:03:24 +0000

    Created Divider Tabs Component

---
+++ src/components/common/DividerTabs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const DividerTabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <div className="flex items-center w-full">
+      {tabs &&
+        tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={`flex justify-center items-center py-2 cursor-pointer transition-all duration-300 rounded-lg ${
+              activeTab === index ? "bg-salmon text-white w-[65%]" : "w-[35%]"
+            }`}
+            onClick={() => setActiveTab(index)}
+          >
+            <p className="text-lg">{tab}</p>
+          </button>
+        ))}
+    </div>
+  );
+};
+
+DividerTabs.propTypes = {
+  tabs: PropTypes.array,
+  activeTab: PropTypes.number,
+  setActiveTab: PropTypes.func,
+};
+
+export default DividerTabs;
