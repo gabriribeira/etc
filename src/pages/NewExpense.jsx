1511c99237f071c6b015337ba76df49fc746d9b7
gabriribeira <gmrribeira@gmail.com>
2024-01-10 18:39:20 +0000

    Started New Expense Page

--- src/pages/NewExpense.jsx
+++ src/pages/NewExpense.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import BottomBar from "../components/common/BottomBar";
+import TopBar from "../components/common/TopBar";
+import Input from "../components/common/Input";
+import Button from "../components/common/Button";
 
 const NewExpense = () => {
-    return (
-      <div>
-        New Expense
-        <BottomBar />
-      </div>
-    );
-  };
-  export default NewExpense;
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  const [date, setDate] = useState("");
+  const [details, setDetails] = useState("");
+  return (
+    <div>
+      <TopBar />
+      <form className="flex flex-col px-5 gap-y-6">
+        <Input label="Title" value={title} onChange={setTitle} />
+        <Input label="Value" value={value} onChange={setValue} />
+        <Input label="Date" value={date} onChange={setDate} />
+        <Input label="Details" value={details} onChange={setDetails} />
+        <Button label="Create Expense" type="submit" className="btn btn-primary" />
+      </form>
+      <BottomBar />
+    </div>
+  );
+};
+export default NewExpense;
