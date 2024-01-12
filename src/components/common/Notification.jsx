cdb2f937163e0895e003540b134f1c5cbfbc2411
Lara Mendes <laramendes@ua.pt>
2024-01-12 21:24:56 +0000

    notifications- search and notifications

---
+++ src/components/common/Notification.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Notification  = () => {
+    return (
+        <><div className="atividade pt-3">
+            <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
+            <div className="descricao-atividade">
+                <p><b>Pedro Silva</b> requested to join <b>Casa da Amizade</b></p>
+                <p className="thin">&middot; 10 minutes ago</p>
+            </div>
+        </div><div className="atividade pt-3">
+                <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
+                <div className="descricao-atividade">
+                    <p><b>Mariana Marques</b> requested to join <b>Casa da Amizade</b></p>
+                    <p className="thin">&middot; 1 hour ago</p>
+                </div>
+            </div>
+            <div className="atividade pt-3">
+                <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
+                <div className="descricao-atividade">
+                    <p><b>Miguel Vidal</b> requested to join <b>Casa da Amizade</b></p>
+                    <p className="thin">&middot; 1 hour ago</p>
+                </div>
+            </div>
+            <div className="atividade pt-3">
+                <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
+                <div className="descricao-atividade">
+                    <p><b>Paulo Ribeira</b> requested to join <b>Casa da Amizade</b></p>
+                    <p className="thin">&middot; 2 hours ago</p>
+                </div>
+            </div>
+            <div className="atividade pt-3">
+                <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
+                <div className="descricao-atividade">
+                    <p><b>Lia Monteiro</b> requested to join <b>Casa da Amizade</b></p>
+                    <p className="thin">&middot; 2 hour ago</p>
+                </div>
+            </div>
+            <div className="atividade pt-3">
+                <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
+                <div className="descricao-atividade">
+                    <p><b>Gabriel Ribeira</b> requested to join <b>Casa da Amizade</b></p>
+                    <p className="thin">&middot; 3 hour ago</p>
+                </div>
+            </div>
+            </>
+  );
+};
+
+export default Notification;
