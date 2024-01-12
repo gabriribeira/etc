20bdb2d5586ca8463a7ae6867a597cfd57ec44c1
Lara Mendes <laramendes@ua.pt>
2024-01-12 13:20:34 +0000

    Homepage-widges and topbar

---
+++ src/components/common/Activity.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../index.css';
+
+const Activity = () => {
+    return (
+        <><div>
+        <p className="pt-5 pl-5"><b>Activity</b></p>
+      </div></>
+    );
+  };
+  
+  export default Activity;
