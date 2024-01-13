e463b5bf029ee3285bebb122ea79a24d4386e82f
gabriribeira <gmrribeira@gmail.com>
2024-01-13 11:04:17 +0000

    Started List Components

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -7,17 +7,46 @@ import TopBar from "../components/common/TopBar";
 import SearchFilter from "../components/common/SearchFilter";
 import { SlArrowRight } from "react-icons/sl";
 import Filter from "../components/common/Filter";
+import Item from "../components/lists/Item";
 
 const List = () => {
   const listsData = ListsData;
   const itemsData = ItemsData;
   const [list, setList] = useState(null);
   const [items, setItems] = useState(null);
+  const [refresh, setRefresh] = useState(true);
   const [newItem, setNewItem] = useState("");
   const location = useLocation();
   const handleFilterClick = () => {
     console.log("Filter Clicked");
   };
+  const handleNewItem = (e) => {
+    e.preventDefault();
+    const newItemJson = {
+      id: itemsData.length + 1,
+      list_id: list.id,
+      name: newItem,
+      price: null,
+      details: "",
+      brand: "",
+      store: "",
+      amount: null,
+      unit: "",
+      members: [],
+      suggestion: false,
+      created_at: new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate(),
+      created_by: null,
+    };
+
+    try {
+      itemsData.push(newItemJson);
+      console.log("Item added successfully!");
+      setRefresh(true);
+      setNewItem("");
+    } catch (error) {
+      console.error("Error adding item:", error);
+    }
+  }
   useEffect(() => {
     if (location.pathname && location.pathname.split("/")[2] && listsData) {
       console.log(location.pathname.split("/")[2]);
@@ -28,11 +57,12 @@ const List = () => {
     }
   }, [location.pathname, listsData]);
   useEffect(() => {
-    if (list && itemsData) {
+    if (list && itemsData && refresh) {
       const items = itemsData.filter((item) => item.list_id === list.id);
       setItems(items);
+      setRefresh(false);
     }
-  }, [list, itemsData]);
+  }, [list, itemsData, refresh]);
   return (
     <div>
       <TopBar />
@@ -44,35 +74,25 @@ const List = () => {
           </div>
           <div className="flex flex-col w-full mt-6 gap-y-3">
             <div className="flex text-white bg-blue px-3 py-4 rounded-2xl w-full justify-between items-center h-full">
-              <div className="flex gap-x-3 w-full items-center h-full">
-                <button className="w-[30px] h-[30px] rounded-full bg-transparent border-2 border-white shrink-0"></button>
+              <form className="flex gap-x-3 w-full items-center h-full" onSubmit={handleNewItem}>
+                <button className="w-[30px] h-[30px] rounded-full bg-transparent border-2 border-white shrink-0" type="button"></button>
                 <input
                   className="text-base font-light bg-transparent w-full placeholder-white placeholder:font-light font-normal focus:border-b-2 focus:border-white focus:outline-none transition-all duration-200 mr-5" 
                   placeholder="Add a new item to the list"
                   value={newItem}
                   onChange={(e) => setNewItem(e.target.value)}
                 />
-              </div>
-              <div className="text-2xl text-white h-full flex items-center">
+              </form>
+              <Link to={`/lists/${list.id}/item/${0}`} className="text-2xl text-white h-full flex items-center">
                 <SlArrowRight />
-              </div>
+              </Link>
             </div>
+            <div className="flex flex-col-reverse gap-y-3">
             {items &&
               items.map((item, index) => (
-                <Link
-                  to={`/lists/${list.id}/item/${item.id}`}
-                  className="flex items-center justify-between bg-blue80 rounded-2xl px-3 py-2 mb-3"
-                  key={index}
-                >
-                  <div className="flex flex-col text-white">
-                    <h1 className="text-xl font-semibold">{item.title}</h1>
-                    <p className="font-light">{item.quantity}</p>
-                  </div>
-                  <div className="text-xl text-white">
-                    <SlArrowRight />
-                  </div>
-                </Link>
+                <Item item={item} key={index} list_id={list.id} />
               ))}
+              </div>
           </div>
         </div>
       )}
