c743247b373a0411bd7017d85345f820275ccaad
gabriribeira <gmrribeira@gmail.com>
2024-01-14 14:29:28 +0000

    Updated Progress Bar on Homepage Widgets

--- src/components/common/ProgressBar.jsx
+++ src/components/common/ProgressBar.jsx
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-const ProgressBar = () => {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // Simulating progress for demonstration purposes
-      if (progress < 60) {
-        setProgress((prevProgress) => prevProgress + 1);
-      } else {
-        clearInterval(interval);
-      }
-    }, 100); // Adjust the interval as needed
-
-    return () => clearInterval(interval);
-  }, [progress]);
-
+const ProgressBar = ({ progress }) => {
   return (
-    <div style={{ width: '80%', margin: 'auto'}}>
-      <progress
-        value={progress}
-        max="100"
-        style={{
-          width: '100%',
-          backgroundColor: '#F1F1F1',
-          borderRadius: '20px',
-        }}
-      >
-        <span style={{ backgroundColor: '#B5D2B6', borderRadius: '8px' }}></span>
-      </progress>
-      <p>{progress}% Complete</p>
+    <div className="w-full h-2 bg-white rounded-full">
+      <div className={`${progress} w-3/4 h-full bg-green60 rounded-full`}></div>
     </div>
   );
+};
+
+ProgressBar.propTypes = {
+  progress: PropTypes.number.isRequired,
 };
 
 export default ProgressBar;
