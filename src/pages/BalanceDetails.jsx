a0f3b2be50f0ef12aab0e20c9774d95d41209ba9
gabriribeira <gmrribeira@gmail.com>
2024-01-15 22:27:14 +0000

    Created "Define as Paid" logic

--- src/pages/BalanceDetails.jsx
+++ src/pages/BalanceDetails.jsx
@@ -1,20 +1,34 @@
-import React, { useEffect } from "react";
-// eslint-disable-next-line
-import PropTypes from "prop-types";
+import React from "react";
 import TopBar from "../components/common/TopBar";
 import BottomBar from "../components/common/BottomBar";
 import ExpenseInBalance from "../components/expenses/ExpenseInBalance";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import Button from "../components/common/Button";
+import ExpensesData from "../data/expenses.json";
 
 const BalanceDetails = () => {
+  const expensesData = ExpensesData;
   const location = useLocation();
-  // eslint-disable-next-line
+  const navigate = useNavigate();
   const { balance, user, expenses } = location.state || {};
-  useEffect(() => {
-    if (location.state) {
-      console.log(location.state.expenses, "ola");
-    }
-  }, [location.state]);
+  const handlePaidBalance = () => {
+    expenses.forEach((expense) => {
+      expensesData.forEach((expenseData) => {
+        if (expenseData.id === expense.id) {
+          if (expenseData.users.length === 1) {
+            expenseData.paid = true;
+          } else {
+            expenseData.value =
+              expenseData.value - expense.value / expenseData.users.length;
+            expenseData.users = expenseData.users.filter(
+              (userIt) => userIt !== user.id
+            );
+          }
+        }
+      });
+    });
+    navigate("/expenses");
+  };
   return (
     balance &&
     user &&
@@ -44,16 +58,29 @@ const BalanceDetails = () => {
             <div className="flex flex-col">
               {balance > 0 ? (
                 <h2 className="font-light text-lg">
-                  You owe <span className="font-semibold">{user.name}</span>
+                  <span className="font-semibold">You</span> owe{" "}
+                  <span className="font-semibold">{user.name}</span>
                 </h2>
               ) : (
                 <h2 className="font-light text-lg">
-                  <span className="font-semibold">{user.name}</span> owes you
+                  <span className="font-semibold">{user.name}</span> owes{" "}
+                  <span className="font-semibold">you</span>
                 </h2>
               )}
-              <h1 className="text-4xl font-semibold">{balance.toFixed(2)}€</h1>
+              <h1 className="text-4xl font-semibold">{Math.abs(balance.toFixed(2))}€</h1>
             </div>
           </div>
+          {balance < 0 ? (
+            <div className="mt-6">
+              <Button label="Define as paid" action={handlePaidBalance} />
+            </div>
+          ) : (
+            <p className="font-light text-black text-sm w-full text-center mt-6">
+              Only {user.name} can define
+              <br />
+              this balance as paid!
+            </p>
+          )}
           <div className="flex flex-col mt-6 gap-y-3">
             <h2 className="text-lg font-semibold">Expenses</h2>
             {expenses &&
@@ -66,10 +93,6 @@ const BalanceDetails = () => {
       </div>
     )
   );
-};
-
-BalanceDetails.propTypes = {
-  location: PropTypes.object.isRequired,
 };
 
 export default BalanceDetails;
