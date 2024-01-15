1c3c85016b5d332ee71eb677fa8fdbce5aabc2e5
gabriribeira <gmrribeira@gmail.com>
2024-01-15 20:38:54 +0000

    Fixed Notifications after merge

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BottomBar from "../components/common/BottomBar";
 import TopBar from "../components/common/TopBar";
 import SearchInput from "../components/common/SearchInput";
-import Notification from "../components/common/Notification";
+import Activity from "../components/common/Activity";
+import ActivityData from "../data/activity.json";
 
 const Notifications = () => {
   const [search, setSearch] = useState("");
@@ -11,16 +12,31 @@ const Notifications = () => {
     setSearch(value);
   };
 
+  const activityData = ActivityData;
+  const [activity, setActivity] = useState([]);
+  useEffect(() => {
+    if (activityData) {
+      setActivity(
+        activityData.filter((activity) => activity.household_id === 1)
+      );
+    }
+  }, [activityData]);
+
   return (
     <div>
       <TopBar />
-      <SearchInput
-        label="Search"
-        value={search}
-        onChange={handleSearchChange}
-        isNotificationSearch={true} 
-      />
-      <Notification/>
+      <div className="flex flex-col px-5 gap-y-3">
+        <SearchInput
+          label="Search"
+          value={search}
+          onChange={handleSearchChange}
+          isNotificationSearch={true}
+        />
+        {activity &&
+          activity.map((activity, index) => (
+            <Activity key={index} activity={activity} />
+          ))}
+      </div>
       <BottomBar />
     </div>
   );
