6416fe5ae6825bf2a41b2822070b23e724da0a04
gabriribeira <gmrribeira@gmail.com>
2024-01-15 20:38:01 +0000

    Updated Activity after merge

--- src/components/common/Activity.jsx
+++ src/components/common/Activity.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import '../../index.css';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import UsersData from "../../data/users.json";
 
-const Activity = () => {
-    return (
-       <div>
-    <p className="pl-5 pt-5 pb-5"><b>Activity</b></p>
-    <div className="atividade">
-        <img src="src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
-        <div className="descricao-atividade">
-            <p><b>Paulo</b> added <b>Cerveja Super Bock</b> to the shopping list.</p>
-            <p className="thin">&middot; 30 minutes ago</p>
-            
-        </div>
+const Activity = ({ activity }) => {
+  const usersData = UsersData;
+  const [member, setMember] = useState(null);
+  useEffect(() => {
+    if (usersData) {
+      setMember(usersData.find((user) => user.id == activity.img));
+    }
+  }, [usersData, activity]);
+  return (
+    member &&
+    <div className="flex items-center w-full text-black gap-x-3">
+      <div className="w-[60px] h-[60px] flex items-center justify-cente relative shrink-0 rounded-full">
+        <img
+          //eslint-disable-next-line
+          src={require(`../../assets/data/users/${member.img}`)}
+          className="w-full h-full object-center top-0 left-0 absolute object-cover rounded-full"
+          alt="User Profile Picture"
+        />
+      </div>
+      <div className="w-full flex flex-col">
+        <p className="text-base">{activity.text}</p>
+        <p className="font-light text-sm">30 minutes ago</p>
+      </div>
     </div>
-    <div className="atividade">
-        <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
-        <div className="descricao-atividade">
-            <p><b>Mariana Marques</b> added <b>Nachos</b> to the shopping list.</p>
-            <p className="thin">&middot; 1 hour ago</p>
-        </div>
-    </div>
-    <div className="atividade">
-        <img src="./src/assets/data/users/lara.webp" className="fotouser" alt="" srcSet="" />
-        <div className="descricao-atividade">
-            <p><b>Gabriel Ribeira</b> started <b>Secret Santa Shopping List</b></p>
-            <p className="thin">&middot; 2 hours ago</p>
-        </div>
-    </div>
-  </div>
-
   );
- };
-  
- export default Activity;
+};
 
+Activity.propTypes = {
+  activity: PropTypes.object.isRequired,
+};
+
+export default Activity;
