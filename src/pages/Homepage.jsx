cb9b2fe7435b1b2d85884c9d7a8d44ed7262f934
gabriribeira <gmrribeira@gmail.com>
2024-01-15 20:36:57 +0000

    Fixed Homepage after merge

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BottomBar from "../components/common/BottomBar";
 import TopBar from "../components/common/TopBar";
 import Widgets from "../components/homepage/Widgets";
 import Activity from "../components/common/Activity";
+import ActivityData from "../data/activity.json";
+import NotificationsLink from "../components/homepage/NotificationsLink";
 
 const Homepage = () => {
+  const activityData = ActivityData;
+  const [activity, setActivity] = useState([]);
+  useEffect(() => {
+    if (activityData) {
+      setActivity(
+        activityData.filter((activity) => activity.household_id === 1)
+      );
+    }
+  }, [activityData]);
   return (
-    <div>
+    <div className="relative">
       <TopBar />
+      <NotificationsLink />
       <div className="flex flex-col px-5">
         <Widgets />
-        <Activity />
+        <div className="flex flex-col mt-6 gap-y-3">
+          <h1 className="font-semibold text-lg">Activity</h1>
+          {activity &&
+            activity.map((activity, index) => (
+              <Activity key={index} activity={activity} />
+            ))}
+        </div>
       </div>
       <BottomBar />
     </div>
