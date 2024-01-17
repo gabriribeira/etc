17f6a82bceebe8becf7329e218e7665973f46613
gabriribeira <gmrribeira@gmail.com>
2024-01-17 22:41:41 +0000

    Debug: Canceled Lint on workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,7 @@
+//eslint-disable-next-line no-undef
 module.exports = {
-	globDirectory: 'build/',
-	globPatterns: [
-		'**/*.{json,ico,html,png,txt,css,js,webp,ttf}'
-	],
-	swDest: 'build/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: "build/",
+  globPatterns: ["**/*.{json,ico,html,png,txt,css,js,webp,ttf}"],
+  swDest: "build/sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
