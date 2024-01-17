79e0526cb4664d64c56ccb87fd4c735adc9c5dbe
gabriribeira <gmrribeira@gmail.com>
2024-01-17 10:49:28 +0000

    Fixed Bottom Bar Index to Allow Overlay

--- src/components/common/BottomBar.jsx
+++ src/components/common/BottomBar.jsx
@@ -14,7 +14,7 @@ const BottomBar = () => {
     <div className="h-[80px]">
 
     </div>
-    <div className="fixed bottom-0 w-screen bg-white flex items-center justify-between text-black px-5 h-[80px] text-4xl z-[100]">
+    <div className="fixed bottom-0 w-screen bg-white flex items-center justify-between text-black px-5 h-[80px] text-4xl z-[90]">
       <Link to="/expenses" className={`flex flex-col items-center leading-5 ${location.pathname === '/expenses' && 'text-blue'}`}><IoWalletOutline /><p className="text-[12px]">Expenses</p></Link>
       <Link to="/lists" className={`flex flex-col items-center leading-5 ${location.pathname === '/lists' && 'text-blue'}`}><TbShoppingCart /><p className="text-[12px]">Lists</p></Link>
       <Link to="/" className={`flex flex-col items-center leading-5 ${location.pathname === '/' && 'text-blue'}`}><GoHome /><p className="text-[12px]">Home</p></Link>
