595976facb6027ad8ab9dc39b58ddc40966aa683
gabriribeira <gmrribeira@gmail.com>
2024-01-17 10:50:03 +0000

    Added Household Overlay Render

--- src/pages/Household.jsx
+++ src/pages/Household.jsx
@@ -5,15 +5,16 @@ import HouseholdsData from "../data/households.json";
 import HouseholdInfo from "../components/household/HouseholdInfo";
 import Members from "../components/household/Members";
 import UsersData from "../data/users.json";
-import { Link } from "react-router-dom";
 import { IoSettingsOutline } from "react-icons/io5";
 import SustainableGoal from "../components/household/SustainableGoal";
+import Overlay from "../components/common/Overlay";
 
 const Household = () => {
   const [household, setHousehold] = useState("");
   const householdsData = HouseholdsData;
   const users = UsersData;
   const [householdUsers, setHouseholdUsers] = useState([]);
+  const [openOverlay, setOpenOverlay] = useState(false);
   useEffect(() => {
     if (householdsData) {
       const households = householdsData.find((household) => household.id === 1); //Não esquecer de mudar o household_id para o id da casa do user
@@ -31,18 +32,31 @@ const Household = () => {
   return (
     household &&
     householdUsers && (
-      <div>
-        <TopBar />
-        <Link to={"/households/1/settings"} className="fixed top-6 right-4 text-4xl text-black z-[101]"> {/* TODO: Não Esquecer Mudar para o id do household correto */}
-          <IoSettingsOutline />
-        </Link>
-        <div className="flex flex-col gap-y-6">
-        <HouseholdInfo household={household} users={householdUsers} />
-        <SustainableGoal />
-        <Members users={householdUsers} admins={household.admins} />
+      <>
+        {openOverlay && (
+          <Overlay
+            label="SETTINGS"
+            options={["Profile", "About"]}
+            links={["/user", "/about"]}
+            hideOverlay={() => setOpenOverlay(false)}
+          />
+        )}
+        <div>
+          <TopBar />
+          <button
+            onClick={() => setOpenOverlay(true)}
+            className="fixed top-6 right-4 text-4xl text-black z-[101]"
+          >
+            <IoSettingsOutline />
+          </button>
+          <div className="flex flex-col gap-y-6">
+            <HouseholdInfo household={household} users={householdUsers} />
+            <SustainableGoal />
+            <Members users={householdUsers} admins={household.admins} />
+          </div>
+          <BottomBar />
         </div>
-        <BottomBar />
-      </div>
+      </>
     )
   );
 };
