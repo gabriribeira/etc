6c146b7b98cde22fb548a91013a92c121b923d62
gabriribeira <gmrribeira@gmail.com>
2024-01-18 13:46:38 +0000

    Started Add Members Page

--- src/components/common/AddMembers.jsx
+++ src/components/common/AddMembers.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProfilePicture from "../../assets/data/users/gabriribeira.webp";
 import { RxPlus } from "react-icons/rx";
+import PropTypes from "prop-types";
 
-const AddMembers = () => {
+const AddMembers = ({ user }) => {
+  const [members, setMembers] = useState([]);
+  useEffect(() => {
+    if (user) {
+      setMembers(members.push(user));
+    }
+  }, [user]);
   return (
     <div className="flex flex-col w-full">
       <label className="mb-2 text-lg font-semibold">Members</label>
       <div className="flex items-center gap-x-2">
         <button className="w-[35px] h-[35px] rounded-full relative flex justify-center items-center">
-          <img src={ProfilePicture} alt="Authenticated User Profile Picture" className="w-full h-full rounded-full absolute object-cover object-center left-0 top-0" />
+          <img
+            src={ProfilePicture}
+            alt="Authenticated User Profile Picture"
+            className="w-full h-full rounded-full absolute object-cover object-center left-0 top-0"
+          />
         </button>
-        <button className="w-[35px] h-[35px] rounded-full relative flex justify-center items-center text-xl bg-black10">
-            <RxPlus />
+        <button
+          type="button"
+          className="w-[35px] h-[35px] rounded-full relative flex justify-center items-center text-xl bg-black10"
+        >
+          <RxPlus />
         </button>
       </div>
     </div>
   );
 };
+
+AddMembers.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
 export default AddMembers;
