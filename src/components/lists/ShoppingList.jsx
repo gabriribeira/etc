2315fd702f89441ef0bc035e5ee30a137abdf48e
gabriribeira <gmrribeira@gmail.com>
2024-01-18 12:56:51 +0000

    Fixed Styling on Shopping List Button

--- src/components/lists/ShoppingList.jsx
+++ src/components/lists/ShoppingList.jsx
@@ -18,8 +18,8 @@ const ShoppingList = ({ list }) => {
       className="flex items-center w-full bg-blue80 justify-between px-3 h-[100px] rounded-2xl"
     >
       <div className="flex flex-col text-white">
-        <h1 className="text-xl font-semibold ">{list.title}</h1>
-        <p className="font-light">{items.length} products</p>
+        <h1 className="text-xl font-medium ">{list.title}</h1>
+        <p className="font-light text-base">{items.length} products</p>
       </div>
       <div className="text-4xl text-white">
         <SlArrowRight />
