22b3ff97524b58de32fa0d7562b16d54bf4cc7ce
gabriribeira <gmrribeira@gmail.com>
2024-01-18 10:09:33 +0000

    Updated Goals Flow

--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import BottomBar from "../components/common/BottomBar";
+import TopBar from "../components/common/TopBar";
+import GoalsData from "../data/goals.json";
+import Button from "../components/common/Button";
 
 const Goals = () => {
-    return (
-      <div>
-        Goals
-        <BottomBar />
+  const goalsData = GoalsData;
+  //eslint-disable-next-line
+  const [tags, setTags] = useState(null);
+  //eslint-disable-next-line
+  const [goal, setGoal] = useState(null);
+  return (
+    <div>
+      <TopBar />
+      <div className="px-5 flex flex-col gap-y-6">
+        <div className="flex flex-col gap-y-3">
+          <h2 className="text-lg font-semibold text-black">
+            Select Household Sustainable Tags
+          </h2>
+          <p className="text-base text-black50 font-medium">
+            Select some tags that you want to define your household in terms of
+            sustainable practices.
+          </p>
+        </div>
+        <div className="flex flex-wrap w-full gap-3">
+          {goalsData &&
+            goalsData.map((goal, index) => (
+              <button
+                key={index}
+                className="rounded-2xl border-2 border-green text-green text-base py-1 px-2"
+              >
+                {goal.title}
+              </button>
+            ))}
+        </div>
+        <div className="w-full flex flex-col items-end gap-y-3">
+          <Button label="Next" action={() => {}} />
+          <a href="/" className="text-base text-blue font-medium">
+            do this later
+          </a>
+        </div>
       </div>
-    );
-  };
-  export default Goals;
+      <BottomBar />
+    </div>
+  );
+};
+export default Goals;
