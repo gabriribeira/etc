451657e48cf2cf6d0f150d9206c26925915630aa
gabriribeira <gmrribeira@gmail.com>
2024-01-18 13:47:29 +0000

    Updated add Members Page after Household Creation

---
+++ src/pages/AddMembers.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { RxPlus } from "react-icons/rx";
+import TopBar from "../components/common/TopBar";
+
+const AddMembers = () => {
+  const [members, setMembers] = useState([]);
+  const [authUser, setAuthUser] = useState(null);
+  // eslint-disable-next-line
+  const [authHousehold, setAuthHousehold] = useState(null);
+  useEffect(() => {
+    const getCookieValue = (cookieName) => {
+      const cookies = document.cookie.split("; ");
+      for (const cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === cookieName) {
+          return JSON.parse(decodeURIComponent(value));
+        }
+      }
+      return null;
+    };
+
+    const storedUser = getCookieValue("user");
+    if (storedUser) {
+      setAuthUser(storedUser);
+    }
+  }, []);
+  useEffect(() => {
+    const getCookieValue = (cookieName) => {
+      const cookies = document.cookie.split("; ");
+      for (const cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === cookieName) {
+          return JSON.parse(decodeURIComponent(value));
+        }
+      }
+      return null;
+    };
+    const storedHousehold = getCookieValue("household");
+    if (storedHousehold) {
+      setAuthHousehold(storedHousehold);
+    }
+  }, []);
+  useEffect(() => {
+    if (authUser) {
+      setMembers(members.push(authUser));
+    }
+  }, [authUser]);
+  return (
+    authUser && (
+      <div className="flex flex-col">
+        <TopBar />
+        <div className="flex flex-col w-full">
+          <label className="mb-2 text-lg font-semibold">Members</label>
+          <div className="flex items-center gap-x-2">
+            <button className="w-[35px] h-[35px] rounded-full relative flex justify-center items-center">
+              <img
+                //eslint-disable-next-line
+                src={require(`../assets/data/users/${authUser.img}`)}
+                alt="Authenticated User Profile Picture"
+                className="w-full h-full rounded-full absolute object-cover object-center left-0 top-0"
+              />
+            </button>
+            <button
+              type="button"
+              className="w-[35px] h-[35px] rounded-full relative flex justify-center items-center text-xl bg-black10"
+            >
+              <RxPlus />
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default AddMembers;
