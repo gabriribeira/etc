595802093830e0890965f023c888dbcb300d99d6
gabriribeira <gmrribeira@gmail.com>
2024-03-10 13:41:11 +0000

    Changed Default Button Styling to Black

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -17,7 +17,7 @@ const Button = ({ label, submit, action, to, bg, lg, stroke }) =>
       onClick={() => action()}
       type={submit ? "submit" : "button"}
       className={`${
-        stroke ? "bg-white" : (bg && bg != '') ? bg : "bg-blue"
+        stroke ? "bg-white" : (bg && bg != '') ? bg : "bg-black90 bg-gradient-to-r from-black90 to-white/20"
       } ${stroke && "border-2 border-blue"} ${stroke ? "text-blue" : "text-white"} rounded-lg ${lg == true ? "py-5" : "py-3"} text-md font-medium w-full focus:outline-none text-center flex justify-center`}
     >
       {label}
