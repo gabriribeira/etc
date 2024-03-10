958c01a1e36062f0c9c638723e5cbfb3c097f6b1
gabriribeira <gmrribeira@gmail.com>
2024-03-10 13:41:31 +0000

    Added Styling to Divider Tabs and Fixed Width

--- src/components/common/DividerTabs.jsx
+++ src/components/common/DividerTabs.jsx
@@ -3,17 +3,17 @@ import PropTypes from "prop-types";
 
 const DividerTabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
-    <div className="flex items-center w-full">
+    <div className="flex items-center justify-between w-full">
       {tabs &&
         tabs.map((tab, index) => (
           <button
             key={index}
-            className={`flex justify-center items-center py-2 cursor-pointer transition-all duration-300 rounded-lg ${
-              activeTab === index ? "bg-salmon text-white w-[65%]" : "w-[35%]"
+            className={`flex justify-center items-center py-2 cursor-pointer transition-all duration-300 rounded-lg shadow-lg ${
+              activeTab === index ? "bg-salmon text-white w-[64%] bg-gradient-to-l from-salmon to-black/20" : "w-[34%] from-white to-black/10 bg-gradient-to-r"
             }`}
             onClick={() => setActiveTab(index)}
           >
-            <p className="text-lg">{tab}</p>
+            <p className={`text-lg ${activeTab === index ? 'font-regular' : 'font-light'}`}>{tab}</p>
           </button>
         ))}
     </div>
