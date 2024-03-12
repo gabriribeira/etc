443454e61243cdd209c85c9608615afcc49fd942
gabriribeira <gmrribeira@gmail.com>
2024-03-12 15:38:58 +0000

    Changed new Button with new icon and placement

--- src/components/common/NewButton.jsx
+++ src/components/common/NewButton.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import { PiPlusThin } from "react-icons/pi";
+import { BsPlusCircleFill } from "react-icons/bs";
 
 const NewButton = ({ path }) => {
   return (
     <Link
       to={path}
-      className="text-[2.5rem] p-5 bg-black90 bg-gradient-to-r from-black90 to-white/30 text-white rounded-full shadow-xl fixed bottom-[12%] right-5 z-[100]"
+      className="text-3xl text-black"
     >
-      <PiPlusThin />
+      <BsPlusCircleFill />
     </Link>
   );
 };
