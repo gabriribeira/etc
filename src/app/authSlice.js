e8a78b12379313252368e649e3f64f111ef95292
gabriribeira <gmrribeira@gmail.com>
2024-05-27 10:00:15 +0100

    Created AuthSlice for authentication state management

---
+++ src/app/authSlice.js
@@ -0,0 +1,51 @@
+// authSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+import api from './api';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    isAuthenticated: null,
+    user: null,
+    currentHouseholdId: null,
+    role: null,
+  },
+  reducers: {
+    setAuthState: (state, action) => {
+      state.isAuthenticated = action.payload.isAuthenticated;
+      state.user = action.payload.user;
+      state.currentHouseholdId = action.payload.currentHouseholdId;
+      state.role = action.payload.roleId;
+    },
+    clearAuthState: (state) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.currentHouseholdId = null;
+      state.role = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(api.endpoints.login.matchFulfilled, (state, action) => {
+        state.isAuthenticated = true;
+        state.user = action.payload;
+        state.currentHouseholdId = action.payload.currentHouseholdId;
+        state.role = action.payload.roleId;
+      })
+      .addMatcher(api.endpoints.googleAuthCallback.matchFulfilled, (state, action) => {
+        state.isAuthenticated = true;
+        state.user = action.payload;
+        state.currentHouseholdId = action.payload.currentHouseholdId;
+        state.role = action.payload.roleId;
+      })
+      .addMatcher(api.endpoints.facebookAuthCallback.matchFulfilled, (state, action) => {
+        state.isAuthenticated = true;
+        state.user = action.payload;
+        state.currentHouseholdId = action.payload.currentHouseholdId;
+        state.role = action.payload.roleId;
+      });
+  },
+});
+
+export const { setAuthState, clearAuthState } = authSlice.actions;
+export default authSlice.reducer;
