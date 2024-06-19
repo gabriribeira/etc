7d0c074d9be0d48e3368960cbc8fac1040831129
gabriribeira <gmrribeira@gmail.com>
2024-06-19 15:39:19 +0100

    Fixed Input Component

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+
 import { BsEye, BsEyeSlash } from "react-icons/bs";
 
 const Input = ({ label, placeholder, value, onChange, error, recommendations, recommendationsAction }) => {
@@ -17,7 +18,7 @@ const Input = ({ label, placeholder, value, onChange, error, recommendations, re
             id={label}
             value={value}
             aria-label={label}
-            onChange={onChange} // Pass the entire event
+            onChange={(e) => onChange(e.target.value)}
             className="border rounded-xl p-2 border-black80 focus:border-black focus:outline-noneplaceholder:text-black50 text-black w-full bg-white"
             placeholder={placeholder ? placeholder : label}
             rows="3"
@@ -26,17 +27,17 @@ const Input = ({ label, placeholder, value, onChange, error, recommendations, re
           <input
             name={label}
             id={label}
-            value={value}
-            aria-label={label}
+            value={value ? value : ""}
+            aria-label= {label}
             type={
               (label === "Password" || label === "Confirm Password") && !showPassword
                 ? "password" : label === "Email" ? "email" : label == "Value" ? "number" : label == "Date" ? "date" : "text"
             }
-            onChange={onChange} // Pass the entire event
+            onChange={(e) => onChange(e.target.value)}
             className={`border rounded-xl p-2 border-black80 h-12 focus:border-black focus:outline-none ${
               (label === "Password" || label === "Confirm Password") &&
               !showPassword &&
-              value !== ""
+              value != ""
                 ? "text-xl"
                 : "text-lg"
             } placeholder:text-black50 text-black w-full bg-white`}
@@ -44,18 +45,20 @@ const Input = ({ label, placeholder, value, onChange, error, recommendations, re
           />
         )}
         {recommendations && (
-          <div className="absolute z-10 w-full rounded-b-xl bg-white shadow-lg max-h-60 overflow-auto top-[100%] left-0 top-0 border-[1px] border-black border-t-0 -mt-3">
-            {recommendations.map((product) => (
-              <div
-                key={product.id}
-                onClick={() => recommendationsAction(product)}
-                className="p-2 hover:bg-gray-100 cursor-pointer"
-              >
-                {product.name}
-              </div>
-            ))}
-          </div>
-        )}
+              <div className="absolute z-10 w-full rounded-b-xl bg-white shadow-lg max-h-60 overflow-auto top-[100%] left-0 top-0 border-[1px] border-black border-t-0 -mt-3">
+              {recommendations.map((product) => (
+                <div
+                  key={product.id}
+                  onClick={() => recommendationsAction(product)}
+                  className="p-2 hover:bg-gray-100 cursor-pointer"
+                >
+                  {product.name}
+                </div>
+              ))}
+            </div>
+        )
+
+        }
         {(label === "Password" || label === "Confirm Password") && (
           <button
             type="button"
@@ -79,7 +82,7 @@ Input.propTypes = {
   onChange: PropTypes.func.isRequired,
   error: PropTypes.string,
   recommendations: PropTypes.array,
-  recommendationsAction: PropTypes.func,
+  recommendationsAction: PropTypes.func
 };
 
 export default Input;
