4211c5eedb042e36d2421ab139355158bc7c6da6
gabriribeira <gmrribeira@gmail.com>
2024-06-21 16:56:17 +0100

    Updated Sustainable Goals Logic throughout the app

--- src/pages/NewHousehold.jsx
+++ src/pages/NewHousehold.jsx
@@ -5,28 +5,28 @@ import Input from "../components/common/Input";
 import Button from "../components/common/Button";
 import BottomBar from "../components/common/BottomBar";
 import ProgressBar from "../components/common/ProgressBar";
-import { useCreateHouseholdMutation, useCreateRequestMutation, useAddHouseholdTagsMutation } from "../app/api";
+import { useCreateHouseholdMutation, useCreateRequestMutation, useAddHouseholdTagsMutation, useGetGoalsByTagsQuery, useAssignGoalsToHouseholdMutation } from "../app/api";
 import { useSelector } from "react-redux";
 import AddMembers from "./AddMembers";
 import CategoriesInput from "../components/common/CategoriesInput";
 import { useNavigate } from "react-router-dom";
-import GoalsData from "../data/goals.json";
 
 const NewHousehold = () => {
-  const goalsData = GoalsData;
+  const user = useSelector((state) => state.auth.user);
   const navigate = useNavigate();
   const [step, setStep] = useState(1);
   const [household, setHousehold] = useState(null);
   const [householdName, setHouseholdName] = useState("");
   const [description, setDescription] = useState("");
+  const [imageFile, setImageFile] = useState(null);
   const [createHousehold, { isLoading, isError }] = useCreateHouseholdMutation();
   const [members, setMembers] = useState([]);
   const [createRequest] = useCreateRequestMutation();
   const [addHouseholdTags] = useAddHouseholdTagsMutation();
   const [tags, setTags] = useState([]);
   const [selectedGoals, setSelectedGoals] = useState([]);
-
-  const user = useSelector((state) => state.auth.user);
+  const { data: fetchedGoals } = useGetGoalsByTagsQuery(tags?.map((tag) => tag.id));
+  const [assignGoals] = useAssignGoalsToHouseholdMutation();
 
   const toggleGoal = (goal) => {
     setSelectedGoals((prevGoals) =>
@@ -37,23 +37,23 @@ const NewHousehold = () => {
   };
 
   const handleNewHousehold = async () => {
-    if (!user) {
-      console.error("No authenticated user found.");
-      return;
-    }
-
-    const newHousehold = {
-      name: householdName,
-      description: description,
-      privacy: false,
-    };
-
     try {
-      let householdResponse = await createHousehold(newHousehold).unwrap();
+      if (!user) {
+        console.error("No authenticated user found.");
+        return;
+      }
+      const formData = new FormData();
+      formData.append("name", householdName);
+      formData.append("description", description);
+      formData.append("privacy", false);
+      if (imageFile) {
+        formData.append("image", imageFile);
+      }
+      let householdResponse = await createHousehold(formData).unwrap();
       setHousehold(householdResponse);
       setStep(2);
     } catch (error) {
-      console.error("Error creating household:", error);
+      console.error("Failed to update household:", error);
     }
   };
 
@@ -76,10 +76,19 @@ const NewHousehold = () => {
 
   const handleTags = async () => {
     try {
-      await addHouseholdTags({ householdId: household.id, tags }).unwrap();
-      navigate("/household")
+      await addHouseholdTags({ householdId: household.id, tags: tags.map((tag) => tag.id) }).unwrap();
+      setStep(4);
     } catch (error) {
       console.error("Error adding tags:", error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await assignGoals({ householdId: household.id, goalIds: selectedGoals.map((goal) => goal.id) }).unwrap();
+      navigate("/household");
+    } catch (error) {
+      console.error('Failed to assign goal:', error);
     }
   };
 
@@ -89,10 +98,18 @@ const NewHousehold = () => {
       {step === 1 && (
         <main className="pt-32">
           <form className="flex flex-1 flex-col w-full h-auto justify-between px-5">
+            <div className="flex flex-col gap-y-3 pb-5">
+              <h2 className="text-lg font-semibold text-black">
+                Create a Household
+              </h2>
+              <p className="text-base text-black50 font-medium">
+                Start by inserting some basic information about your household.
+              </p>
+            </div>
             <ProgressBar progress={1} />
             <div className="flex flex-col w-full gap-y-6 h-auto">
               <div className="my-4">
-                <ImageUpload />
+                <ImageUpload onImageUpload={setImageFile} />
               </div>
               <Input
                 label="Household Name"
@@ -124,6 +141,14 @@ const NewHousehold = () => {
       {step === 2 && (
         <main className="pt-32">
           <form className="flex flex-1 flex-col w-full h-auto justify-between px-5">
+            <div className="flex flex-col gap-y-3">
+              <h2 className="text-lg font-semibold text-black">
+                Invite Members
+              </h2>
+              <p className="text-base text-black50 font-medium">
+                Invite some members to join your household.
+              </p>
+            </div>
             <ProgressBar progress={5} />
             <div className="flex flex-col w-full gap-y-6 h-auto">
               <AddMembers
@@ -146,7 +171,7 @@ const NewHousehold = () => {
       {step === 3 && (
         <main className="pt-32">
           <form className="flex flex-1 flex-col w-full h-auto justify-between px-5">
-            <div className="flex flex-col gap-y-3">
+            <div className="flex flex-col gap-y-2 pb-5">
               <h2 className="text-lg font-semibold text-black">
                 Sustainable Tags
               </h2>
@@ -154,8 +179,8 @@ const NewHousehold = () => {
                 Select some tags that you want to define your household in terms of sustainable practices.
               </p>
             </div>
-            <ProgressBar progress={9} />
-            <div className="flex flex-col w-full gap-y-6 h-auto">
+            <ProgressBar progress={7} />
+            <div className="flex flex-col w-full gap-y-6 h-auto pt-5">
               <CategoriesInput label={"Sustainable Tags"} categorySelected={tags} onChange={setTags} filter={false} />
             </div>
           </form>
@@ -171,40 +196,34 @@ const NewHousehold = () => {
       {step === 4 && (
         <main className="pt-32">
           <form className="flex flex-1 flex-col w-full h-auto justify-between px-5">
-            <div className="flex flex-col gap-y-3">
+            <div className="flex flex-col gap-y-2 mb-5">
               <h2 className="text-lg font-semibold text-black">
-                Sustainable Tags
+                Sustainable Goals
               </h2>
               <p className="text-base text-black50 font-medium">
-                Select some tags that you want to define your household in terms of sustainable practices.
+                Select a goals that you want to achieve as a household in terms of sustainable practices.
               </p>
             </div>
             <ProgressBar progress={9} />
-            <div className="flex flex-col w-full gap-y-6 h-auto">
-              {goalsData &&
-                goalsData.map((goal, slide, index) => (
+            <div className="flex flex-col w-full gap-y-6 h-auto mt-5">
+              {fetchedGoals && fetchedGoals.data &&
+                fetchedGoals.data.map((goal, slide, index) => (
                   <button
                     key={index}
-                    className={`flex flex-col rounded-2xl border-2 border-green p-5 text-left gap-y-3 m-y-2 ${selectedGoals.includes(goal.goal)
+                    className={`flex flex-col rounded-2xl border-2 border-green p-5 text-left gap-y-3 m-y-2 ${selectedGoals.includes(goal)
                       ? "bg-green text-white"
                       : "text-green"
                       }`}
-                    onClick={() => toggleGoal(goal.goal)}
+                    onClick={() => toggleGoal(goal)}
+                    type="button"
                   >
-                    <p>tags</p>
-
+                    <p>{goal.amount} times in 1 month</p>
                     <div className="flex flex-col">
-                      <h2 className="text-xs font-normal text-black">{goal.goal.slug}</h2>
+                      <h2 className="text-xs font-normal text-black">{goal.slug}</h2>
                       <h1 className="font-semibold text-2xl text-black">
-                        {goal.goal.title}
+                        {goal.title}
                       </h1>
-                      <p className="font-normal text-sm text-black">{goal.goal.details}</p>
-                    </div>
-                    <div className="flex flex-col mb-3">
-                      <p className="text-black font-normal text-xs mb-1">
-                        métrica
-                      </p>
-                      <ProgressBar />
+                      <p className="font-normal text-sm text-black">{goal.details}</p>
                     </div>
                   </button>
                 ))}
@@ -213,7 +232,7 @@ const NewHousehold = () => {
           <div className="h-full my-6 px-5">
             <Button
               label="Next"
-              action={handleTags}
+              action={handleSubmit}
               isLoading={isLoading}
             />
           </div>
