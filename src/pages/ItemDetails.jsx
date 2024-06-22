5d58feadc9f5ffd650e542e34df35d75416d03e7
gabriribeira <gmrribeira@gmail.com>
2024-06-22 16:02:21 +0100

    Updated Item Details with Image Upload

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -64,32 +64,33 @@ const ItemDetails = () => {
   }, [item]);
 
   const handleSaveItem = async () => {
-    const itemData = {
-      list_id: Number(listId),
-      category_id: category ? category : null,
-      name,
-      price: Number(value),
-      details,
-      brand,
-      store,
-      amount,
-      unit,
-      members,
-      img_url: photo,
-      is_suggestion: isSuggestions,
-      is_expense: false,
-    };
-
     try {
+      const formData = new FormData();
+      formData.append("list_id", Number(listId));
+      formData.append("category_id", category ? category : null);
+      formData.append("name", name);
+      formData.append("price", Number(value));
+      formData.append("details", details);
+      formData.append("brand", brand);
+      formData.append("store", store);
+      formData.append("amount", amount);
+      formData.append("unit", unit);
+      formData.append("members", members);
+      if (photo) {
+        formData.append("img_url", photo);
+      }
+      formData.append("is_suggestion", isSuggestions);
+      formData.append("is_expense", false);
       if (itemId === "0") {
-        await addItem(itemData).unwrap();
+        await addItem(formData).unwrap();
       } else {
-        await updateItem({ id: itemId, ...itemData }).unwrap();
+        await updateItem({ id: itemId, formData }).unwrap();
       }
       await refetchListItems();
       navigate(`/lists/${listId}`);
     } catch (error) {
-      console.error("Error saving item:", error);
+      console.error("Failed to update Item:", error);
+      alert("Failed to update Item");
     }
   };
 
@@ -119,14 +120,16 @@ const ItemDetails = () => {
       <main className="pt-32 mb-16 gap-x-5">
         <div className="flex justify-start px-5 gap-3 mb-5">
           <PhotoInput onChange={setPhoto} value={photo} />
-          <div className="relative">
-            <Input
-              label="Name *"
-              value={name}
-              placeholder="Name"
-              onChange={handleNameChange}
-              required
-            />
+          <div className="relative w-full">
+            <div className="w-full">
+              <Input
+                label="Name *"
+                value={name}
+                placeholder="Name"
+                onChange={handleNameChange}
+                required
+              />
+            </div>
             {showRecommendations && recommendations && (
               <div className="absolute bg-white border border-gray-200 shadow-lg w-full mt-1 z-10">
                 {recommendations.map((product) => (
